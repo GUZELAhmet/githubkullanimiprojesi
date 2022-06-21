0e3e97d45fd5634a1d638b643537a8ea09441e52
AhmetGUZEL <gzlhmt@gmail.com>
2022-06-21 23:56:50 +0300

    ilkversiyon

--- src/kisiselKullanım/Githubkullanimi1.java
+++ src/kisiselKullanım/Githubkullanimi1.java
@@ -3,5 +3,6 @@ package kisiselKullanım;
 public class Githubkullanimi1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("that's it");
     }
 }
